8eed55d1cc9c801a83e7843848ebae91f4437600
Reinaldo Cardoso <38083439+reinaldo89@users.noreply.github.com>
2018-12-20 15:35:53 +0000

    Add Global Site Tag (gtag.js) 📈

---
+++ js/ga.js
@@ -0,0 +1,8 @@
+function loadtracking() {
+    window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'UA-104993254-3', {'anonymize_ip': true });
+}
+
+loadtracking();
